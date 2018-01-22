32addcf16ee7baad1c8e641ebc5687627d4f3102
za-chujun <1571970439@qq.com>
2018-01-22 19:56:45 +0800

    add MybatisGenerator main entry

---
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/MybatisGenerator.java
@@ -0,0 +1,42 @@
+/**
+ *    Copyright 2006-2016 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.generator;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MybatisGenerator {
+
+    public static void main(String[] args) throws Exception, XMLParserException {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(MybatisGenerator.class.getClassLoader().getResourceAsStream(
+                MybatisGenerator.class.getPackage().getName().replaceAll("\\.", "/") + "/" + "cfg_bpm_for_mac_idea.xml"));
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+        System.out.println("end");
+
+        //        System.out.println(System.getProperties());
+    }
+}
