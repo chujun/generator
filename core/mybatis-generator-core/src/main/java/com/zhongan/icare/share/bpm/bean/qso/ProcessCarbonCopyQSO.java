ecb3ce76d8e22e273c97bfda3916cf931146caf7
za-chujun <1571970439@qq.com>
2018-03-09 17:56:43 +0800

    modify

--- core/mybatis-generator-core/src/main/java/com/zhongan/icare/share/bpm/bean/qso/ProcessCarbonCopyQSO.java
+++
@@ -1,8 +0,0 @@
-package com.zhongan.icare.share.bpm.bean.qso;
-
-import com.zhongan.icare.share.bpm.dto.ProcessCarbonCopyDTO;
-
-@lombok.Getter
-@lombok.Setter
-public class ProcessCarbonCopyQSO extends ProcessCarbonCopyDTO {
-}
